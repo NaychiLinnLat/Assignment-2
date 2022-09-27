fb9eca063b1f86744029654b4a12a2e9784b98b1
NaychiLinnLat <naychilinnlatt5@gmail.com>
2022-09-27 09:32:42 +0630

    add route to app.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const { isAbsolute } = require("path");
 const app = express();
 
 app.listen(3000, () => {
     console.log("App started and listening on port 3000");
 
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", (req,res) => {
+    res.sendFile(__dirname + "/about.html");
+});
+
+app.get("/contact", (req,res) => {
+    res.sendFile(__dirname + "/contact.html")
 });
